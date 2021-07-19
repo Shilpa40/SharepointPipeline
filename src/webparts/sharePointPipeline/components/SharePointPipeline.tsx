591dc7fe25ca6c35afff5e3c12936371dce2bde2
shilpa <shilpa@nagarro.com>
2021-07-19 16:56:29 +0530

    Again text change

--- src/webparts/sharePointPipeline/components/SharePointPipeline.tsx
+++ src/webparts/sharePointPipeline/components/SharePointPipeline.tsx
@@ -10,7 +10,7 @@ export default class SharePointPipeline extends React.Component<ISharePointPipel
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to Shilpa First SPFx Project using Azure Devops and Gihub Project!</span>
+              <span className={ styles.title }>Welcome to First SPFx Project using Azure Devops and Github Project!</span>
               <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
               <p className={ styles.description }>{escape(this.props.description)}</p>
               <a href="https://aka.ms/spfx" className={ styles.button }>
